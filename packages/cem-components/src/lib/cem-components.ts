5d53baeeef6b0ee7eb8ad2e5ab72abe0a806c640
Sasha Firsov <suns@firsov.net>
2026-01-03 08:59:52 -0800

    1st draft nx sub-repos

--- packages/cem-components/src/lib/cem-components.ts
+++ packages/cem-components/src/lib/cem-components.ts
@@ -1,3 +1,9 @@
+import { cemTheme } from '@epa-wg/cem-theme';
+
 export function cemComponents(): string {
   return 'cem-components';
+}
+
+export function getTheme(): string {
+  return cemTheme();
 }
