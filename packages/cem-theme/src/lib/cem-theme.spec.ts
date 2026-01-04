748bd84a9f977e1fa7766ea29a0fb0b00ea3042a
Sasha Firsov <suns@firsov.net>
2026-01-03 20:33:36 -0800

    fix: repo `version` added into package.json on all sub-projects

--- packages/cem-theme/src/lib/cem-theme.spec.ts
+++ packages/cem-theme/src/lib/cem-theme.spec.ts
@@ -1,7 +1,8 @@
 import { cemTheme } from './cem-theme.js';
+import packageJson from '../../package.json' with { type: 'json' };
 
 describe('cemTheme', () => {
   it('should work', () => {
-    expect(cemTheme()['@epa-wg/cem-theme']).toEqual('0.0.0');
+    expect(cemTheme()['@epa-wg/cem-theme']).toEqual(packageJson.version); // i.e. > '0.0.1'
   });
 });
--- packages/cem-theme/src/lib/cem-theme.ts
+++ packages/cem-theme/src/lib/cem-theme.ts
@@ -1,3 +1,5 @@
+import packageJson from '../../package.json' with { type: 'json' };
+
 export function cemTheme(): Record<string, string> {
-  return {"@epa-wg/cem-theme": "0.0.0"};
+  return {"@epa-wg/cem-theme": packageJson.version};
 }
