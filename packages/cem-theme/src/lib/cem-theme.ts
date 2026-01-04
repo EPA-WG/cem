1c34012b6fc70c8a02e0b70784479b5440475b60
Sasha Firsov <suns@firsov.net>
2026-01-03 22:47:26 -0800

    fix: bump version 0.0.6

--- packages/cem-theme/src/lib/cem-theme.ts
+++ packages/cem-theme/src/lib/cem-theme.ts
@@ -1,5 +1,5 @@
 import packageJson from '../../package.json' with { type: 'json' };
 
 export function cemTheme(): Record<string, string> {
-  return {"@epa-wg/cem-theme": packageJson.version, type: packageJson.type, versionBump: '0.0.5'};
+  return {"@epa-wg/cem-theme": packageJson.version, type: packageJson.type, versionBump: '0.0.6'};
 }
